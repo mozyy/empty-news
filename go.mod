d2b24a877f0972284b7bc734b005ff25724683bf
yyue <yy@yyue.dev>
2021-07-31 01:59:59 +0000

    add oauth2, docker-compose

--- go.mod
+++ go.mod
@@ -3,19 +3,28 @@ module github.com/mozyy/empty-news
 go 1.15
 
 require (
-	github.com/PuerkitoBio/goquery v1.6.1 // indirect
+	github.com/PuerkitoBio/goquery v1.7.0 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
 	github.com/antchfx/htmlquery v1.2.3 // indirect
-	github.com/antchfx/xmlquery v1.3.5 // indirect
+	github.com/antchfx/xmlquery v1.3.6 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/go-oauth2/oauth2/v4 v4.3.0
+	github.com/go-session/session v3.1.2+incompatible // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/micro/micro/v3 v3.0.0
+	github.com/micro/micro/v3 v3.3.0
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20210415231046-e915ea6b2b7d // indirect
-	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
-	google.golang.org/genproto v0.0.0-20210416161957-9910b6c460de // indirect
-	google.golang.org/grpc v1.37.0 // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/tidwall/buntdb v1.2.4 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210624195500-8bfb893ecb84 // indirect
+	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 )
--- main.go
+++ main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
-	"github.com/mozyy/empty-news/handler"
 	"log"
 	"net"
+	"strings"
+
+	"github.com/mozyy/empty-news/handler"
 
 	pb "github.com/mozyy/empty-news/proto/news"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
-const (
-	prot = 50051
+var (
+	errMissingMetadata = status.Errorf(codes.InvalidArgument, "missing metadata")
+	errInvalidToken    = status.Errorf(codes.Unauthenticated, "invalid token")
 )
+var port = flag.Int("port", 50051, "the port to serve on")
 
 func main() {
 	// Create service
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", prot))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	opts := []grpc.ServerOption{
+		// The following grpc.ServerOption adds an interceptor for all unary
+		// RPCs. To configure an interceptor for streaming RPCs, see:
+		// https://godoc.org/google.golang.org/grpc#StreamInterceptor
+		grpc.UnaryInterceptor(ensureValidToken),
+		// // Enable TLS for all incoming connections.
+		// grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
+	}
+
+	grpcServer := grpc.NewServer(opts...)
 	// Register handler
 	pb.RegisterNewsServer(grpcServer, new(handler.NewsStruct))
 	log.Printf("starting")
@@ -31,14 +49,34 @@ func main() {
 	} else {
 		log.Printf("Server started successfully")
 	}
-	// Run service
-	// if err := srv.Run(); err != nil {
-	// 	logger.Fatal(err)
-	// }
-	// crawler.News(func(list []*empty.NewsItem) {
-	// 	fmt.Println("123123")
-	// 	fmt.Println(list)
-	// })
-	// fmt.Println("2222")
+}
 
+// valid validates the authorization.
+func valid(authorization []string) bool {
+	if len(authorization) < 1 {
+		return false
+	}
+	token := strings.TrimPrefix(authorization[0], "Bearer ")
+	// Perform the token validation here. For the sake of this example, the code
+	// here forgoes any of the usual OAuth2 token validation and instead checks
+	// for a token matching an arbitrary string.
+	return token == "some-secret-token" || true
+}
+
+// ensureValidToken ensures a valid token exists within a request's metadata. If
+// the token is missing or invalid, the interceptor blocks execution of the
+// handler and returns an error. Otherwise, the interceptor invokes the unary
+// handler.
+func ensureValidToken(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errMissingMetadata
+	}
+	// The keys within metadata.MD are normalized to lowercase.
+	// See: https://godoc.org/google.golang.org/grpc/metadata#New
+	if !valid(md["authorization"]) {
+		return nil, errInvalidToken
+	}
+	// Continue execution of handler after ensuring a valid token.
+	return handler(ctx, req)
 }
