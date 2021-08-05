50a78d6f3e5c97cb6545da4c0dcda9c987e8baac
yyue <yy@yyue.dev>
2021-08-05 15:36:18 +0000

    test mysql

--- services/db/db.go
+++ services/db/db.go
@@ -2,17 +2,26 @@ package db
 
 import (
 	"database/sql"
+	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type DB struct {
-	db *sql.DB
+	Db *sql.DB
 }
 
-func (d DB) init() {
-	db, err := sql.Open("mysql", "user:password@/dbname")
+var Database = DB{}
+
+func init() {
+	Database.Init()
+}
+
+func (d DB) Init() {
+	// environment variable mysql_dsn:
+	// https://github.com/go-sql-driver/mysql#dsn-data-source-name
+	db, err := sql.Open("mysql", os.Getenv("mysql_dsn")+"e_user?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
@@ -20,5 +29,16 @@ func (d DB) init() {
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
-	d.db = db
+	d.Db = db
+}
+func Init() *sql.DB {
+	db, err := sql.Open("mysql", os.Getenv("mysql_dsn")+"e_user?parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	return db
 }
---
+++ services/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mozyy/empty-news/services/db"
+)
+
+func main() {
+	// user := struct{id }{}
+	var Database = db.Init()
+	// res, err := Database.Exec("INSERT user SET mobile=182813,password=123456;")
+	// checkErr(err)
+	// fmt.Println(res.RowsAffected())
+	rows, err := Database.Query("select * from user;")
+	checkErr(err)
+	t, err := rows.ColumnTypes()
+	checkErr(err)
+	v := []map[string]interface{}{}
+	for _, tt := range t {
+		fmt.Println(tt, &tt)
+	}
+	for rows.Next() {
+		vv := map[string]interface{}{}
+		var vvv, vvvv, vvvvv interface{}
+		rows.Scan(&vvv, &vvvv, &vvvvv)
+		vv[t[0].Name()] = vvv
+		vv[t[1].Name()] = vvvv
+		vv[t[2].Name()] = vvvvv
+		v = append(v, vv)
+	}
+	fmt.Println(v)
+}
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
